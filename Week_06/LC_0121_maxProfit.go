43ffd60b8f1f819a8ec280e348966925071b681c
cherry77-cloud <cherry1615405@gmail.com>
2025-03-11 16:26:21 +0800

    Delete Week_06 directory

--- Week_06/LC_0121_maxProfit.go
+++
@@ -1,8 +0,0 @@
-func maxProfit(prices []int) int {
-    minPrice, ans := prices[0], 0
-    for _, price := range prices {
-        minPrice = min(minPrice, price)
-        ans = max(ans, price - minPrice)
-    }
-    return ans
-}
