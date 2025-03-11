5b38a227efa9a745893bba7a7251e540a3ab0a9f
cherry77-cloud <cherry1615405@gmail.com>
2025-03-11 16:25:46 +0800

    Delete Week_02 directory

--- Week_02/LC_0053_maxSubArray.go
+++
@@ -1,9 +0,0 @@
-func maxSubArray(nums []int) int {
-    ans := math.MinInt    // 注意答案可以是负数，不能初始化成 0
-    f := 0
-    for _, x := range nums {
-        f = max(f, 0) + x
-        ans = max(ans, f)
-    }
-    return ans
-}
