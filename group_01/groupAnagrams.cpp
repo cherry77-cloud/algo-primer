3aae628d2ed4064263eaeeaebdb8f89913b07bf6
cherry77-cloud <cherry1615405@gmail.com>
2025-06-07 11:50:54 +0800

    Create groupAnagrams.cpp

---
+++ group_01/groupAnagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagram;
+        for (auto &str : strs) {
+            string sorted_str = str;
+            ranges::sort(sorted_str);
+            anagram[sorted_str].emplace_back(str);
+        }
+
+        vector<vector<string>> ans;
+        for (auto& [_, val] : anagram) {
+            ans.emplace_back(std::move(val));
+        }
+        return ans;
+    }
+};
