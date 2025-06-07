96b5e79d3f4909aeb3ce5561ab9569c180b8d73a
cherry77-cloud <cherry1615405@gmail.com>
2025-06-07 11:59:47 +0800

    Update groupAnagrams.cpp

--- group_01/groupAnagrams.cpp
+++ group_01/groupAnagrams.cpp
@@ -4,10 +4,10 @@ public:
         unordered_map<string, vector<string>> anagram;
         for (auto &str : strs) {
             string sorted_str = str;
-            ranges::sort(sorted_str);
+            ranges::sort(sorted_str);  // ranges::sort (C++20), 需要 #include <ranges> 或 <algorithm>
             anagram[sorted_str].emplace_back(str);
         }
-
+        
         vector<vector<string>> ans;
         for (auto& [_, val] : anagram) {
             ans.emplace_back(std::move(val));
